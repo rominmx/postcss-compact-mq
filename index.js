2d32527ecfe40375dea3563b31584ef6953feb7c
Maxim Romin <maxim.romin@gmail.com>
2016-07-18 10:36:39 -0400

    es5; removed typo

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var postcss = require('postcss'),
     MQValue = require('./lib/mediaqueries').MQValue,
     parseMQ = require('./lib/mediaqueries').parseMQ;
 
-module.exports = postcss.plugin('postcss-alias-list', opts => {
+module.exports = postcss.plugin('postcss-compact-mq', function(opts) {
     var text = opts && opts.text || 'no text';
 
-    return (css) => {
+    return function(css) {
         var breakpoints, mediaqueries;
 
         // parse and clean breakpoints at-rules
         breakpoints = [];
-        css.walkAtRules('breakpoints', atRule => {
-            atRule.walkDecls(decl => {
+        css.walkAtRules('breakpoints', function(atRule) {
+            atRule.walkDecls(function(decl) {
                 breakpoints.push({
                     prop: decl.prop,
                     value: decl.value
@@ -23,8 +23,8 @@ module.exports = postcss.plugin('postcss-alias-list', opts => {
 
         // parse and clean mediaqueries at-rules
         mediaqueries = [];
-        css.walkAtRules('media-queries', atRule => {
-            atRule.walkDecls(decl => {
+        css.walkAtRules('media-queries', function(atRule) {
+            atRule.walkDecls(function(decl) {
                 mediaqueries.push({
                     prop: decl.prop,
                     value: decl.value
@@ -34,12 +34,12 @@ module.exports = postcss.plugin('postcss-alias-list', opts => {
             atRule.remove();
         });
 
-        css.walkAtRules('media', atRule => {
+        css.walkAtRules('media', function(atRule) {
             var parsedValues = [];
             var params = atRule.params;
 
             // search and remove mediaquery aliases
-            mediaqueries.some(mq => {
+            mediaqueries.some(function(mq) {
                 if (mq.prop == atRule.params) {
                     params = mq.value;
                 }
@@ -47,7 +47,7 @@ module.exports = postcss.plugin('postcss-alias-list', opts => {
             
             // at-rule contains <, >, =
             if (params.search(/[<=>]/) != -1) {
-                postcss.list.comma(params).forEach(param => {
+                postcss.list.comma(params).forEach(function(param) {
                     var sign, 
                         value, 
                         units, 
@@ -62,7 +62,7 @@ module.exports = postcss.plugin('postcss-alias-list', opts => {
                     bpAliasIndex = -1;
 
                     // string contains property from breakpoints at-rule
-                    breakpoints.some((element, i) => {
+                    breakpoints.some(function(element, i) {
                         if (element.prop == breakpoint) {
                             bpAliasIndex = i;
                         }
